fd561b56e388498173f2118b65af65cafeaed24d
ehulewicz <eahtree@gmail.com>
2026-01-06 05:01:23 -0600

    backend schema

---
+++ backend/schema.sql
@@ -0,0 +1,87 @@
+-- brothers
+CREATE TABLE brother (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    points INTEGER NOT NULL DEFAULT 0,
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+-- brother roles
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+-- duty types
+CREATE TABLE duty_type (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+-- list of possible duties
+CREATE TABLE duty_definition (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    duty_type_id INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    FOREIGN KEY (duty_type_id) REFERENCES duty_type(id)
+);
+
+-- event types
+CREATE TABLE event_type (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+-- default event duties
+CREATE TABLE event_type_duty (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_type_id INTEGER NOT NULL,
+    duty_definition_id INTEGER NOT NULL,
+    default_points INTEGER NOT NULL,
+    default_required_brothers INTEGER NOT NULL,
+    FOREIGN KEY (event_type_id) REFERENCES event_type(id),
+    FOREIGN KEY (duty_definition_id) REFERENCES duty_definition(id)
+);
+
+-- events on calendar
+CREATE TABLE event (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    event_type_id INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL,
+    FOREIGN KEY (event_type_id) REFERENCES event_type(id)
+);
+
+-- all duties for a specific event (default and not)
+CREATE TABLE event_duty (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_id INTEGER NOT NULL,
+    duty_definition_id INTEGER NOT NULL,
+    points INTEGER NOT NULL,
+    required_brothers INTEGER NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES event(id),
+    FOREIGN KEY (duty_definition_id) REFERENCES duty_definition(id)
+);
+
+-- assigned duties
+CREATE TABLE event_duty_assignment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_duty_id INTEGER NOT NULL,
+    brother_id INTEGER NOT NULL,
+
+    status_id INTEGER NOT NULL DEFAULT 1,
+    completed_at TEXT,
+    rejected_at TEXT,
+    rejection_reason TEXT,
+
+    FOREIGN KEY (event_duty_id) REFERENCES event_duty(id),
+    FOREIGN KEY (brother_id) REFERENCES brother(id)
+);
+
+CREATE TABLE event_duty_assignment_status (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
